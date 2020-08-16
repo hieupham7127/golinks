7616fe3c9eb5e3f4ee926162a3392d9f2c507db1
Quynh Do <dndquynh@gmail.com>
2020-08-16 16:35:26 -0700

    Add delete feature

--- popup/popup.js
+++ popup/popup.js
@@ -3,6 +3,12 @@ const urlInput = document.getElementById("full-link")
 const storedUrl = document.getElementById("stored-url")
 const saveButton = document.getElementById("save")
 const searchQuery = document.getElementById("search")
+const mainDiv = document.getElementById("main")
+const optionsDiv = document.getElementById("options")
+const optionButton = document.getElementById("option-btn")
+const homeButton = document.getElementById("home-btn")
+const itemList = document.getElementById("item-list")
+const addSection = document.getElementById("addSection");
 
 golinkInput.oninput = async () => {
     const golink = golinkInput.value
@@ -39,35 +45,82 @@ saveButton.onclick = () => {
 browser.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true})
     .then(tabs => urlInput.value = tabs[0].url);
 
-searchQuery.onkeyup = () => {
-    const template = document.getElementById("autocomplete");
-    const baseURL = "https://www.google.com/s2/favicons?sz=64&domain_url=";
+optionButton.addEventListener("click", function() {
+    mainDiv.style.display = "none";
+    optionsDiv.style.display = "block";
+    optionButton.style.display = "none";
+    homeButton.style.display = "block";
+});
+
+homeButton.addEventListener("click", function() {
+    mainDiv.style.display = "block";
+    optionsDiv.style.display = "none";
+    optionButton.style.display = "block";
+    homeButton.style.display = "none";
+});
+
+async function getAllEntries() {
     try {
-        browser.storage.sync.get(null)
+        await browser.storage.sync.get(null)
         .then(items => {
-            let keys = Object.keys(items);
             const entries = Object.entries(items);
             let allItems = {};
             entries.forEach(entry => {
+                console.log(entry);
                 allItems[entry[0]] = entry[1].url;
+                const searchEntry = createEntry(entry);
+                $(searchEntry).appendTo(itemList);
             });
-            $("#search").autocomplete({
-                source: keys,
-            })
-            .autocomplete("instance")._renderItem = function (ul, item) {
-                let searchEntry = template.content.cloneNode(true);
-                const url = allItems[item.value];
-                searchEntry.querySelector("#shortcut").textContent = item.value;
-                searchEntry.querySelector("#url").textContent = url;
-                searchEntry.querySelector("#icon").src = baseURL + url;
-                searchEntry.querySelector("li").addEventListener("click", function() {
-                    browser.tabs.create({active: true, url: url});
-                });
-                return $(searchEntry)
-                    .appendTo( ul );
-            };
         })
     } catch (e) {
         alert("Failed! Error: " + e.toString());
     }
 }
+
+function createEntry(entry) {
+    const template = document.getElementById("autocomplete");
+    const baseURL = "https://www.google.com/s2/favicons?sz=64&domain_url=";
+    let searchEntry = template.content.cloneNode(true);
+    const url = entry[1].url;
+    const trashIcon = searchEntry.querySelector("#trash-icon");
+    searchEntry.querySelector("#shortcut").textContent = entry[0];
+    searchEntry.querySelector("#url").textContent = url;
+    searchEntry.querySelector("#icon").src = baseURL + url;
+    searchEntry.querySelector("li").addEventListener("click", function() {
+        browser.tabs.create({active: true, url: url});
+    });
+    searchEntry.querySelector("li").addEventListener("mouseover", function() {
+        trashIcon.style.display = "block";
+    });
+    searchEntry.querySelector("li").addEventListener("mouseleave", function() {
+        trashIcon.style.display = "none";
+    });
+    trashIcon.addEventListener("click", function(event) {
+        let response = confirm("Delete shortcut " + entry[0] + "?");
+        if (response) {
+            browser.storage.sync.remove(entry[0]);
+        }
+        event.stopPropagation();
+    });
+    return searchEntry;
+}
+
+$(document).ready(getAllEntries());
+
+browser.storage.onChanged.addListener(() => {
+    $(itemList).empty();
+    getAllEntries()
+});
+
+$(document).ready(function(){
+    $(searchQuery).on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#item-list li").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+});
+
+$("#add").on("click", function() {
+    addSection.style.display = "block";
+});
