c0dfb291f4b5d577d1ab59fd99d5bc630900168b
Quynh Do <35310169+dndquynh@users.noreply.github.com>
2020-09-03 23:17:38 -0700

    Fix search engine redirection issue (#18)
    - Redirect search link to user's stored website
    Co-authored-by: Hieu Pham <phamhuuquanghieu@gmail.com>

--- linkify.js
+++ linkify.js
@@ -15,10 +15,34 @@ function redirectGoLink(details) {
     return {redirectUrl: local_storage_cache[golink].url}
 }
 
+/**
+ * Extract the go link from the search link and redirect to the user's stored website if exists.
+ */
+function redirectSearchLink(details) {
+    let golink = details.url.replace(/(.*)\:\/\/(.*)=go%2F/, "")
+    // Global flag g is included to replace all "%2F" with "/"
+    golink = golink.replace(/%2F/g, "/");
+
+    // Remove trailing query string content of the search link
+    // Yahoo: &fr=opensearch, Ecosia: &addon=opensearch
+    golink = golink.replace(/\&fr=opensearch|\&addon=opensearch/, "")
+
+    if (!local_storage_cache[golink]) {
+        return;
+    }
+    return {redirectUrl: local_storage_cache[golink].url}
+}
+
 browser.storage.onChanged.addListener(() => getLocalStorageData());
 
 browser.webRequest.onBeforeRequest.addListener(
     redirectGoLink,
     { "urls": ["*://go/*"] },
+    ["blocking"],
+);
+
+browser.webRequest.onBeforeRequest.addListener(
+    redirectSearchLink,
+    { "urls": ["*://*.google.com/*=go%2F*", "*://*.yahoo.com/*=go%2F*", "*://*.bing.com/*=go%2F*", "*://*.duckduckgo.com/*=go%2F*", "*://*.ecosia.org/*=go%2F*", "*://*.baidu.com/*=go%2F*"] },
     ["blocking"],
 );
